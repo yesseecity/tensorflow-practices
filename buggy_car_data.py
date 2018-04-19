ac70f74e58c6a41a32705ba2127abfe56e5e494b
yesseecity <yesseecity@gmail.com>
2018-04-19 16:09:38 +0800

    Add init files

---
+++ buggy_car_data.py
@@ -0,0 +1,165 @@
+import os
+import pandas as pd
+import tensorflow as tf;
+csvColumnNames = ['imageFolder', 'imageName', 'throOUT', 'rudoOUT']
+
+def load_train_data():
+	trainPath = os.getcwd()+"/train-data";
+	trainCSVs = trainPath + '/csv';
+	trainX = [];
+	trainY = [];
+	tempX = [];
+
+	## train data
+	for root, dirs, files in os.walk(trainCSVs):
+		for file in files:
+			csv = pd.read_csv(trainCSVs+'/'+file, names=csvColumnNames);
+			tempX += list(csv['imageFolder']+'/'+csv['imageName']);
+			trainY += list(csv['rudoOUT']);
+
+	for idx, imgPath in enumerate(tempX):
+		a1 = os.getcwd() + '/train-data/' +os.path.normpath(imgPath);
+		a1 = a1.replace('/home/pi/myProject/python/rc-tracked-car/', '')
+		# a1 = a1.replace('images/', 'images_L_bmp/')
+		# "/home/pi/myProject/python/rc-tracked-car/images/2018-03-21-17.10.25","img_2018-03-21-17.10.27.652620.jpg",49,360
+
+		if not os.path.isfile(a1):
+		# if not os.path.exists(a1):
+			print(idx, a1 , 'not exist');
+			print(os.path.normpath(imgPath));
+			break;
+		trainX.append(a1);
+	# print(len(trainX))
+	# print(len(trainY))
+	
+	return (trainX, trainY);
+
+def load_test_data():
+	testPath = os.getcwd()+"/test-data";
+	testCSVs = testPath + '/csv';
+	testX = [];
+	testY = [];
+	tempX = [];
+
+	## train data
+	for root, dirs, files in os.walk(testCSVs):
+		for file in files:
+			csv = pd.read_csv(testCSVs+'/'+file, names=csvColumnNames);
+			tempX += list(csv['imageFolder']+'/'+csv['imageName']);
+			testY += list(csv['rudoOUT']);
+
+	for idx, imgPath in enumerate(tempX):
+		a1 = os.getcwd() + '/test-data/' +os.path.normpath(imgPath);
+		a1 = a1.replace('/home/pi/myProject/python/rc-tracked-car/', '')
+		# a1 = a1.replace('images/', 'images_L_bmp/')
+
+		if not os.path.isfile(a1):
+		# if not os.path.exists(a1):
+			print(idx, a1 , 'not exist');
+			print(os.path.normpath(imgPath));
+			break;
+		testX.append(a1);
+	# print(len(testX))
+	# print(len(testY))
+	
+	return (testX, testY);
+
+def get_train_csv():
+	trainPath = os.getcwd()+"/train-data";
+	trainCSVs = trainPath + '/csv';
+
+	returnFiles = [];
+	for root, dirs, files in os.walk(trainCSVs):
+		for file in files:
+			returnFiles.append(trainCSVs + '/' + file);
+	
+	return returnFiles;
+
+def get_test_csv():
+	testPath = os.getcwd()+"/test-data";
+	testCSVs = testPath + '/csv';
+
+	returnFiles = [];
+	for root, dirs, files in os.walk(testCSVs):
+		for file in files:
+			returnFiles.append(testCSVs + '/' + file);
+	
+	return returnFiles;
+
+
+def _decode_img(filename, label):
+	#print(type(filename));
+	#print(dir(filename));
+	image_string = tf.read_file(filename)
+	image_decoded = tf.image.decode_bmp(
+		image_string,
+		channels=0,
+		name='decode_bmp')
+	image_decoded.set_shape([128, 96, 1]);
+	image_decoded = tf.cast(image_decoded, tf.float32, name='image')
+	#print(image_decoded);
+	#image_resized = tf.image.resize_image_with_crop_or_pad(image_decoded, 128, 96)
+	#image_resized = tf.image.resize_images(image_decoded, [128, 96])
+	
+	
+	return image_decoded, label
+
+
+def _images_L_bmp(filename):
+	filename = filename.replace('images/', 'images_L_bmp/');
+	filename = filename.replace('.jpg', '.jpg.bmp');
+	return filename;
+
+def features():
+	(trainX, trainY) = load_train_data();
+	return set(trainY);
+
+def train_dataset():
+	(trainX, trainY) = load_train_data()
+	trainX = list(map(_images_L_bmp, trainX))
+	filenames = tf.constant(trainX, name='filenames');
+	labels = tf.constant(trainY, name='labels');
+	dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+	dataset = dataset.map(_decode_img)
+	return dataset;
+
+def test_dataset():
+	(testX, testY) = load_test_data()
+	testX = list(map(_images_L_bmp, testX))
+	filenames = tf.constant(testX);
+	labels = tf.constant(testY);
+	dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+	dataset = dataset.map(_decode_img)
+	return dataset;
+
+
+def train_input_fn(batch_size):
+	"""An input function for training"""
+	dataset = train_dataset();
+	dataset = dataset.shuffle(batch_size).repeat().batch(batch_size);
+	return dataset
+
+def test_input_fn(batch_size):
+	"""An input function for evaluation or prediction"""
+	dataset = test_dataset();
+	dataset = dataset.shuffle(batch_size).batch(batch_size);
+	return dataset
+
+if __name__ == '__main__':
+	dataset = train_input_fn(50);
+	'''
+	TypeError: 
+	<BatchDataset 
+		shapes: (<unknown>, (?,)), 
+		types: (tf.uint8, tf.int32)
+	>
+	is not a callable object
+
+
+	'''
+	print();
+	print(dataset);
+	#print(dir(dataset)) ;
+	#print('output_classes: ', dataset.output_classes);
+	#print('output_shapes: ', dataset.output_shapes);
+
---
+++ train.py
@@ -0,0 +1,198 @@
+import tensorflow as tf;
+import numpy as np;
+import buggy_car_data;
+
+batch_size = 10
+
+def cnn_model_fn(features, labels, mode):
+  #"""Model function for CNN."""
+  # Input Layer
+  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+  # buggy_car_data images are 128x96 pixels, and have one color channel
+  input_layer = tf.reshape(features, [-1, 128, 96, 1])
+  #input_layer = tf.reshape(features, [batch_size, 128, 96, 1])
+
+  # Convolutional Layer #1
+  # Computes 32 features using a 5x5 filter with ReLU activation.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 128, 96, 1]
+  # Output Tensor Shape: [batch_size, 128, 96, 32]
+  conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #1
+  # First max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 128, 96, 32]
+  # Output Tensor Shape: [batch_size, 64, 48, 32]
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+  # Convolutional Layer #2
+  # Computes 64 features using a 5x5 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 64, 48, 32]
+  # Output Tensor Shape: [batch_size, 64, 48, 64]
+  conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #2
+  # Second max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 64, 48, 64]
+  # Output Tensor Shape: [batch_size, 32, 24, 64]
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+  # Convolutional Layer #3
+  # Computes 64 features using a 5x5 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 32, 24, 64]
+  # Output Tensor Shape: [batch_size, 32, 24, 128]
+  conv3 = tf.layers.conv2d(
+      inputs=pool2,
+      filters=128,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #3
+  # Second max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 32, 24, 128]
+  # Output Tensor Shape: [batch_size, 16, 12, 128]
+  pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
+
+  # Convolutional Layer #4
+  # Computes 64 features using a 5x5 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 16, 12, 128]
+  # Output Tensor Shape: [batch_size, 16, 12, 256]
+  conv4 = tf.layers.conv2d(
+      inputs=pool3,
+      filters=256,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #4
+  # Second max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 16, 12, 256]
+  # Output Tensor Shape: [batch_size, 8, 6, 256]
+  pool4 = tf.layers.max_pooling2d(inputs=conv4, pool_size=[2, 2], strides=2)
+
+  # Convolutional Layer #5
+  # Computes 64 features using a 5x5 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 8, 6, 256]
+  # Output Tensor Shape: [batch_size, 8, 6, 512]
+  conv5 = tf.layers.conv2d(
+      inputs=pool4,
+      filters=512,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #5
+  # Second max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 8, 6, 512]
+  # Output Tensor Shape: [batch_size, 4, 3, 512]
+  pool5 = tf.layers.max_pooling2d(inputs=conv5, pool_size=[2, 2], strides=2)
+
+  # Flatten tensor into a batch of vectors
+  # Input Tensor Shape: [batch_size, 4, 3, 512]
+  # Output Tensor Shape: [batch_size, 4 * 3 * 512]
+  pool5_flat = tf.reshape(pool5, [-1, 4 * 3 * 512])
+
+  # Dense Layer
+  # Densely connected layer with 1024 neurons
+  # Input Tensor Shape: [batch_size, 4 * 3 * 512]
+  # Output Tensor Shape: [batch_size, 2048]
+  dense = tf.layers.dense(inputs=pool5_flat, units=2048, activation=tf.nn.relu)
+
+  # Add dropout operation; 0.6 probability that element will be kept
+  dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+  # Logits layer
+  # Input Tensor Shape: [batch_size, 2048]
+  # Output Tensor Shape: [batch_size, 21]
+  logits = tf.layers.dense(inputs=dropout, units=21)
+
+  predictions = {
+      # Generate predictions (for PREDICT and EVAL mode)
+      "classes": tf.argmax(input=logits, axis=1),
+      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+      # `logging_hook`.
+      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+  }
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    print('return PREDICT');
+    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+  # Calculate Loss (for both TRAIN and EVAL modes)
+  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+  print('labels: ', labels);
+  print('logits: ', logits);
+  print('loss', loss);
+
+  # Configure the Training Op (for TRAIN mode)
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=tf.train.get_global_step())
+    print('return TRAIN');
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+  # Add evaluation metrics (for EVAL mode)
+  eval_metric_ops = {
+      "accuracy": tf.metrics.accuracy(
+          labels=labels, predictions=predictions["classes"])}
+  print('return EstimatorSpec');
+  return tf.estimator.EstimatorSpec(
+      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+def main(unused_argv):
+	# Load training and eval data
+	train_input_fn = buggy_car_data.train_input_fn(batch_size);
+	eval_input_fn = buggy_car_data.test_input_fn(batch_size);
+
+	# Create the Estimator
+	buggy_car_classifier = tf.estimator.Estimator(
+		model_fn=cnn_model_fn,
+		model_dir="./models")
+
+	# Set up logging for predictions
+	# Log the values in the "Softmax" tensor with label "probabilities"
+	tensors_to_log = {"probabilities": "softmax_tensor"}
+	logging_hook = tf.train.LoggingTensorHook(
+		tensors=tensors_to_log,
+		every_n_iter=50)
+
+	# Train the model
+	buggy_car_classifier.train(
+		#input_fn=train_input_fn,
+		input_fn= lambda:buggy_car_data.train_input_fn(batch_size),
+		#steps=20000,
+		steps=1,
+		hooks=[logging_hook])
+
+	# Evaluate the model and print results
+	eval_results = buggy_car_classifier.evaluate(input_fn=eval_input_fn)
+	print(eval_results)
+
+
+
+
+if __name__ == "__main__":
+	#config = tf.ConfigProto(allow_soft_placement=True);
+	#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5);
+	#config.gpu_options.allow_growth=True;
+	
+	tf.app.run(main)
+  
